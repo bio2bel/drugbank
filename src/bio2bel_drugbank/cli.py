a2df2869f92d6691d147e295565c0874fa4de9e6
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-04-22 13:27:02 +0200

    Add output of Patent information to CLI

--- src/bio2bel_drugbank/cli.py
+++ src/bio2bel_drugbank/cli.py
@@ -2,10 +2,33 @@
 
 """Run this script with :code:`python3 -m bio2bel_drugbank`"""
 
+import click
+
 from bio2bel import build_cli
 from .manager import Manager
 
 main = build_cli(Manager)
+
+
+@main.group()
+def manage():
+    pass
+
+
+@manage.group()
+def patents():
+    pass
+
+
+@patents.command()
+@click.pass_obj
+def ls(manager):
+    """Lists patents"""
+    click.echo_via_pager('\n'.join(
+        f'{patent.patent_id}\t{patent.country}\t{"|".join(drug.name for drug in patent.drugs)}'
+        for patent in manager.list_patents()
+    ))
+
 
 if __name__ == '__main__':
     main()
